8bde220228e5950344b6e9a4d44db142131ce433
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2021-09-10 22:09:17 -0500

    Update dash.py

--- application/dash.py
+++ application/dash.py
@@ -122,7 +122,7 @@ femi15_21 = ff[[
  'Enero20','Febrero20','Marzo20','Abril20','Mayo20','Junio20','Julio20',
  'Agosto20','Septiembre20','Octubre20','Noviembre20','Diciembre20',
     
- 'Enero21','Febrero21','Marzo21','Abril21','Mayo21','Junio21',#'Julio21',
+ 'Enero21','Febrero21','Marzo21','Abril21','Mayo21','Junio21','Julio21',
 # 'Agosto21','Septiembre21','Octubre21','Noviembre21','Diciembre21'
              ]]
 
@@ -149,7 +149,7 @@ femi15_21['Total2020']= femi15_21[[ 'Enero20', 'Febrero20', 'Marzo20', 'Abril20'
                                'Noviembre20', 'Diciembre20',]].sum(axis=1)
 
 femi15_21['Total2021']= femi15_21[[ 'Enero21','Febrero21', 'Marzo21', 'Abril21', 'Mayo21',
-                                   'Junio21',#'Julio21','Agosto21','Septiembre21','Octubre21',
+                                   'Junio21','Julio21',#'Agosto21','Septiembre21','Octubre21',
                                    #'Noviembre21','Diciembre21'
                                   ]].sum(axis=1)
 
@@ -184,7 +184,7 @@ pagra = ff[[
  'Enero20', 'Febrero20', 'Marzo20', 'Abril20', 'Mayo20', 'Junio20', 'Julio20', 'Agosto20',
     'Septiembre20','Octubre20', 'Noviembre20', 'Diciembre20',
 
- 'Enero21', 'Febrero21', 'Marzo21','Abril21', 'Mayo21', 'Junio21',# 'Julio21', 'Agosto21',
+ 'Enero21', 'Febrero21', 'Marzo21','Abril21', 'Mayo21', 'Junio21', 'Julio21', #'Agosto21',
   #  'Septiembre21','Octubre21','Noviembre21','Diciembre21'
             ]]
 
@@ -536,7 +536,7 @@ body = html.Div([
                        dbc.Badge("mensuales", color="info", className="mr-1")]), 
                                        width={'size': 11,  "offset":1 })]),
        dbc.Row([        
-               dbc.Col(html.H5("(hasta junio 2021)"),
+               dbc.Col(html.H5("(hasta julio 2021)"),
                                        width={ 'size': 3, "offset":1 }),
 
             ]),
@@ -609,21 +609,7 @@ body = html.Div([
     html.Br(),
     
                 dbc.Row([
-          dbc.Col(dbc.Button(([html.P("Guanajuato", style={"font-size": 30,"color": "black","background-color": "white"}),
-                       dbc.CardImg(src="https://github.com/fdealbam/Incesto/blob/main/application/static/gto.jpeg?raw=true"),
-    
-                       html.P(
-                           "Los 10 municipios con más incesto fueron: Irapuato (2), Pénjamo (1), San Felipe (1), Celaya (1),"
-                           " León (1), Jerécuaro (1), Jaral del Progreso (1), Cortazar (1), Santa Catarina (0)"
-                           " y San Diego de la Unión (0).",
-                           style={'font-size': 14, "font-family":"Arial", "text-align":"justify" }),
-               ]), style={"background-color":"white",
-                         "box-shadow": "10px 20px 30px black",
-                         'margin-left': '300px',
-                        'width': '550px',
-                         
-                         }, disabled=True)),
-                       
+                               
                dbc.Col(dbc.Button(([html.P("Baja California", style={"font-size": 30,"color": "black","background-color": "white"}),
                        dbc.CardImg(src="https://github.com/fdealbam/Incesto/blob/main/application/static/bc.jpeg?raw=true"),
                      html.Br(),
@@ -636,10 +622,25 @@ body = html.Div([
                            style={'font-size': 14, "font-family":"Arial", "text-align":"justify" }),
                ]), style={"background-color":"white",
                          "box-shadow": "10px 20px 30px black",
-                        # 'margin-left': '10px',
+                         'margin-left': '300px',
                         'width': '550px',
                         
                          }, disabled=True)),
+          dbc.Col(dbc.Button(([html.P("Guanajuato", style={"font-size": 30,"color": "black","background-color": "white"}),
+                       dbc.CardImg(src="https://github.com/fdealbam/Incesto/blob/main/application/static/gto.jpeg?raw=true"),
+    
+                       html.P(
+                           "Los 10 municipios con más incesto fueron: Irapuato (2), Pénjamo (1), San Felipe (1), Celaya (1),"
+                           " León (1), Jerécuaro (1), Jaral del Progreso (1), Cortazar (1), Santa Catarina (0)"
+                           " y San Diego de la Unión (0).",
+                           style={'font-size': 14, "font-family":"Arial", "text-align":"justify" }),
+               ]), style={"background-color":"white",
+                         "box-shadow": "10px 20px 30px black",
+                        # 'margin-left': '300px',
+                        'width': '550px',
+                         
+                         }, disabled=True)),
+            
                      html.Br(),
           ]),
   
@@ -782,8 +783,8 @@ app.layout = html.Div([body],
                                     "background-color": "lightgray"}
                                     )
 
-from application.dash import app
-from settings import config
+#from application.dash import app
+#from settings import config
 
 if __name__ == "__main__":
     app.run_server()
